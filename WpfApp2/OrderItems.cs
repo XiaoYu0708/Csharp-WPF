645f66ab856f54fba129baedbcc354770c0b7d0a
XiaoYu <68182187+XiaoYu0708@users.noreply.github.com>
2023-05-28 02:39:12 +0800

    Add files via upload

---
+++ WpfApp2/OrderItems.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp2
+{
+    internal class OrderItems
+    {
+        public int Index { get; set; }
+        public int Quantity { get; set; }
+        public int SubTotal { get; set; }
+    }
+}

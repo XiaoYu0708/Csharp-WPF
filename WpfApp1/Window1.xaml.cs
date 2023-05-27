c6939fae79bf15ae70725e29b06344b0b4084d51
XiaoYu <68182187+XiaoYu0708@users.noreply.github.com>
2023-05-28 02:35:48 +0800

    Add files via upload

---
+++ WpfApp1/Window1.xaml.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Window1.xaml 的互動邏輯
+    /// </summary>
+    public partial class Window1 : Window
+    {
+        int num1=0,num2=0,num3=0;
+        public Window1()
+        {
+            InitializeComponent();
+        }
+
+        private void Slider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            lb1.Content = ((int)sl1.Value);
+            num1 = ((int)sl1.Value);
+            sum();
+        }
+
+        private void Slider_ValueChanged_1(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            lb2.Content = ((int)sl2.Value);
+            num2 = ((int)sl2.Value);
+            sum();
+        }
+
+        private void Slider_ValueChanged_2(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            lb3.Content = ((int)sl3.Value);
+            num3 = ((int)sl3.Value);
+            sum();
+        }
+
+        private void sum()
+        {
+            double sum = num1*0.3 + num2*0.3 + num3*0.4;
+
+            if(sum < 60)
+            {
+                textblock1.Background = new SolidColorBrush(Colors.Pink);
+            }
+            else
+            {
+                textblock1.Background = new SolidColorBrush(Colors.LightGreen);
+            }
+            textblock1.Text = $"國文0.3：{num1}\n英文0.3：{num2}\n數學0.4：{num3}\n平均：{sum.ToString("0.0")}";
+        }
+    }
+}
---
+++ WpfApp1/Window3.xaml.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Window3.xaml 的互動邏輯
+    /// </summary>
+    public partial class Window3 : Window
+    {
+        public Window3()
+        {
+            InitializeComponent();
+        }
+
+        int have=100,num,type,type_c;
+        string player, com,x;
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            num = integerupdown.Value.Value;
+            if (num <= 0 || num > have)
+            {
+                MessageBox.Show("下注金額不得小於0\n或下注金額不得大於剩餘賭金");
+                return;
+            }
+            type = 0;//剪刀
+            math();
+
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            num = integerupdown.Value.Value;
+            if (num <= 0 || num > have)
+            {
+                MessageBox.Show("下注金額不得小於0\n或下注金額不得大於剩餘賭金");
+                return;
+            }
+            type = 1;//石頭
+            math();
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            num = integerupdown.Value.Value;
+            if (num <= 0 || num > have)
+            {
+                MessageBox.Show("下注金額不得小於0\n或下注金額不得大於剩餘賭金");
+                return;
+            }
+            type = 2;//布
+            math();
+        }
+
+        private void math()
+        {
+            var rand = new Random();
+            type_c = rand.Next(3);
+            if(type == 0)//剪刀
+            {
+                player = "剪刀";
+                if (type_c == 0)
+                { //剪刀
+                    com = "剪刀";
+                    textbox.Background = new SolidColorBrush(Colors.LightGray);//平手
+                    x = "平手";
+                }
+                if (type_c == 1)
+                { //石頭
+                    com = "石頭";
+                    textbox.Background = new SolidColorBrush(Colors.LightPink);//電腦贏
+                    have = have - num;
+                    x = "電腦贏";
+                }
+                if (type_c == 2)
+                { //布
+                    com = "布";
+                    textbox.Background = new SolidColorBrush(Colors.LightGreen);//玩家贏
+                    have = have + num;
+                    x = "玩家贏";
+                }
+            }
+            if (type == 1)//石頭
+            {
+                player = "石頭";
+                if (type_c == 0)
+                { //剪刀
+                    com = "剪刀";
+                    textbox.Background = new SolidColorBrush(Colors.LightGreen);//玩家贏
+                    have = have + num;
+                    x = "玩家贏";
+                }
+                if (type_c == 1)
+                { //石頭
+                    com = "石頭";
+                    textbox.Background = new SolidColorBrush(Colors.LightGray);//平手
+                    x = "平手";
+                }
+                if (type_c == 2)
+                { //布
+                    com = "布";
+                    textbox.Background = new SolidColorBrush(Colors.LightPink);//電腦贏
+                    have = have - num;
+                    x = "電腦贏";
+                }
+            }
+            if (type == 2)//布
+            {
+                player = "布";
+                if (type_c == 0)
+                { //剪刀
+                    com = "剪刀";
+                    textbox.Background = new SolidColorBrush(Colors.LightPink);//電腦贏
+                    have = have - num;
+                    x = "電腦贏";
+                }
+                if (type_c == 1)
+                { //石頭
+                    com = "石頭";
+                    textbox.Background = new SolidColorBrush(Colors.LightGreen);//玩家贏
+                    have = have + num;
+                    x = "玩家贏";
+                }
+                if (type_c == 2)
+                { //布
+                    com = "布";
+                    textbox.Background = new SolidColorBrush(Colors.LightGray);//平手  
+                    x = "平手";
+                }
+            }
+            textbox.Text = $"玩家出{player}，電腦出{com}，{x}。\n此次下注{num}元，賭金剩餘{have}";
+        }
+
+    }
+}
